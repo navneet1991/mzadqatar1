2f7fcc5fa778b93959d5ebe9c5ee7c2194b163c2
mohgood <maboelgoud@ebdaadt.com>
2016-08-03 10:52:17 +0200

    Updates

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mzadqatar'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'test'),
+            'password' => env('DB_PASSWORD', 'Mohammed@1234'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
